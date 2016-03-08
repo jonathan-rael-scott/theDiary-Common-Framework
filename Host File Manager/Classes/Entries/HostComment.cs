d95ce58fc40c017809f2ecdfbeb64286e3dec5f7
priestofpsi <priestofpsi@gmail.com>
2016-03-08 22:26:57 +0200

    Inclussion of missing files from previous commit

---
+++ Host File Manager/Classes/Entries/HostComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theDiary.Tools.Development.HostFileManager
+{
+    public sealed class HostComment
+           : IEntry
+    {
+        public HostComment(string text)
+        {
+            this.EntryId = Guid.NewGuid();
+            this.LineText = text;
+        }
+        public Guid EntryId
+        {
+            get; private set;
+        }
+        public string LineText
+        {
+            get; private set;
+        }
+    }
+}
---
+++ Host File Manager/Classes/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace theDiary.Tools.Development.HostFileManager
+{
+    internal static class Extensions
+    {
+        public static System.Windows.Forms.ImageList CreateImageList(this System.Windows.Forms.ImageList list, Type resourceType, System.Drawing.Size size)
+        {
+            if (list == null)
+                list = new ImageList();
+            list.Images.Clear();
+            list.ImageSize = size;
+
+            var properties = resourceType.GetProperties().ToArray();
+            Dictionary<string, System.Drawing.Image> images = new Dictionary<string, System.Drawing.Image>();
+            properties.Where(property => property.PropertyType.IsSubclassOf(typeof(System.Drawing.Image))).ForEach(prop => list.Images.Add(prop.Name, prop.GetValue(null, null).As<System.Drawing.Image>()));
+
+            return list;
+        }
+    }
+}
